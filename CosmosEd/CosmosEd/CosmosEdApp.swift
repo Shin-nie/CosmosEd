e94020c508328f1001e7a697ef254b92b04a6ced
Shin-nie <thuhangvu0302@gmail.com>
2024-09-04 14:15:58 +1000

    adding XCode Project

---
+++ CosmosEd/CosmosEd/CosmosEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosEdApp.swift
+//  CosmosEd
+//
+//  Created by Hang Vu on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CosmosEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
