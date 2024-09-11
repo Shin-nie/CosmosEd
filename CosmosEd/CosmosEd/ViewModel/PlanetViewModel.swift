955a7f8b98f265aeb06379ee12a3b1c1c559f597
Shin-nie <thuhangvu0302@gmail.com>
2024-09-11 13:11:59 +1000

    Icon App

--- CosmosEd/CosmosEd/ViewModel/PlanetViewModel.swift
+++ CosmosEd/CosmosEd/ViewModel/PlanetViewModel.swift
@@ -4,46 +4,46 @@
 
 import Combine
 import Foundation
-
-class PlanetViewModel: ObservableObject {
-    @Published var planet: Planet?
-
-    func fetchPlanetData(name: String) {
-        let apiKey = "ZtK3G/gPhJPPevcqPAjtwQ==lqklW02aFzQ4C68y"
-        let urlString = "https://api.api-ninjas.com/v1/planets?name=\(name)"
-        
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.setValue(apiKey, forHTTPHeaderField: "X-Api-Key")
-        
-        // Perform the API call
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                print("Error fetching data: \(error)")
-                return
-            }
-            
-            guard let data = data else {
-                print("No data returned")
-                return
-            }
-            
-            do {
-                // Decode the JSON response
-                let planets = try JSONDecoder().decode([Planet].self, from: data)
-                DispatchQueue.main.async {
-                    self.planet = planets.first
-                }
-            } catch {
-                print("Error decoding data: \(error)")
-            }
-        }.resume()
-    }
-}
+//
+//class PlanetViewModel: ObservableObject {
+//    @Published var planet: Planet?
+//
+//    func fetchPlanetData(name: String) {
+//        let apiKey = "ZtK3G/gPhJPPevcqPAjtwQ==lqklW02aFzQ4C68y"
+//        let urlString = "https://api.api-ninjas.com/v1/planets?name=\(name)"
+//        
+//        guard let url = URL(string: urlString) else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        var request = URLRequest(url: url)
+//        request.setValue(apiKey, forHTTPHeaderField: "X-Api-Key")
+//        
+//        // Perform the API call
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let error = error {
+//                print("Error fetching data: \(error)")
+//                return
+//            }
+//            
+//            guard let data = data else {
+//                print("No data returned")
+//                return
+//            }
+//            
+//            do {
+//                // Decode the JSON response
+//                let planets = try JSONDecoder().decode([Planet].self, from: data)
+//                DispatchQueue.main.async {
+//                    self.planet = planets.first
+//                }
+//            } catch {
+//                print("Error decoding data: \(error)")
+//            }
+//        }.resume()
+//    }
+//}
 
 //struct ContentView: View {
 //    @StateObject var viewModel = PlanetViewModel()
